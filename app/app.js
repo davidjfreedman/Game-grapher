f47701c7f48a8a77f8c48055be1d9d5de8651f60
David Freedman <davidjfreedman@gmail.com>
2014-09-29 13:03:10 -0500

    data is functional, working on chart functionality

--- app/app.js
+++ app/app.js
@@ -22,102 +22,227 @@ function app() {
     };
 
     KotakuSearch.prototype.DateCheck = function(date) {
-        var articleDay = date.substr(0, date.indexOf(' '););
-        var actualWeekdays = [
-            "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-        ]
-        var setdate = new Date();
-        var dateToday = setdate.getDay(); // 5
-        var dayToday = actualWeekdays[dateToday]; // Friday
-        var articleWeekdays = new Array(6);
-        articleWeekdays[0] = dayToday;
-        var dayToSet = dateToday - 1;
-        if (dayToSet < 0) {
-            dayToSet = 6;
-        }
-        for (i = 1; i <= 6; i++) {
-            if (dayToSet < 0) {
-                dayToSet = 6;
+        var articleDateSubstring = date.substr(0, date.indexOf(' '));
+
+        if (!(isNaN(date.charAt(0)))) { // if the date is a number
+            return articleDateSubstring; // return date portion (not time)
+        } else if (articleDateSubstring === "Today") {
+            var d = new Date();
+            var returnedDate = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
+            return returnedDate;
+        } else if (isNaN(date.charAt(0)) && articleDateSubstring !== "Today") {
+            var actualWeekdays = [
+                "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+            ]; //weekdays as normally seen in JS
+            var dayToday = actualWeekdays[new Date().getDay()]; // Sunday
+            var articleWeekdays = new Array(6); // setting new Array based around today
+            articleWeekdays[0] = dayToday; // setting today as the first day
+            var dayToSet = new Date().getDay() - 1;
+            for (i = 1; i <= 6; i++) { // 'i' is the array spot
+                if (dayToSet < 0) { // if we're going into last week...
+                    dayToSet = 6; // ...sets day as Saturday
+                };
+                articleWeekdays[i] = actualWeekdays[dayToSet]; //sets weekday in new spot on array
+                dayToSet = dayToSet - 1; // moves back a day
             };
-            articleWeekdays[i] = actualWeekdays[dayToSet];
-            dayToSet = dayToSet - 1;
+            var workingDate = new Date(); //sunday
+            var dateInMonth = new Date().getDate(); // 28th
+            var dayDifference = articleWeekdays.indexOf(articleDateSubstring); // -4
+            workingDate.setDate(dateInMonth - dayDifference);
+            var actualDate = (workingDate.getMonth() + 1) + "/" + workingDate.getDate() + "/" + workingDate.getFullYear();
+            return actualDate;
         };
-        return 0 - articleWeekdays.indexOf(date);
-        articles have string days
-        turn days into integer difference from Today
-        set today value as actual date string
-        set value of all other string dates as actual date string
-
-
-
-
-
-
-
-
-
-        // if (date.indexOf("Today") > -1) {
-        //     // get date in July 28, 2010 format
-        // } else if (articleDay === "Sunday" || articleDay === "Monday" || articleDay === "Tuesday" || articleDay === "Wednesday" || articleDay === "Thursday" || articleDay === "Friday" || articleDay === "Saturday") {
-        //     var dayToday = new Date();
-        //     var dayToday = dayToday.getDay();
-        //     var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        //     weekday.forEach(function() {
-        //         if (articleDay === weekday) {
-        //             var articleDay = index;
-        //         }
-        //     });
-        //     var dayDifference = dayToday - articleDay; //-1
-
-        //    	}
     };
-}
 
-KotakuSearch.prototype.ShowResults = function(query) {
-    $.when(
-        this.TagSearch(query)
-    ).then(
-        function(results) {
-            console.log(results);
-            var data = results.results.TagSearch;
-            $('.resultsAmount')[0].innerText = "There are " + data.length + " results for this search.";
-            console.log(data);
-            console.log(data[0]);
-            var arraySpot = 0;
-            var arrayDate = data[0].Date.text;
-            if (isNaN(date.charAt(1)))) {
-                return;
-            }
-            for (i = 0, i < (data.length - 1), i++) {
-            	
-            }
-            console.log(arrayDate);
 
-            // var  a for array spot
-            // var vardate = first date based on selector (ie month, week, day, etc)
-            // for(i<array.length, i=0, i++) {
-            // 	get that pageview result
-            // 	get date in correct format
-            // 	if daily(
-            // 		if dateofpost === vardate
-            // 		array[a] += pageview)
-            // 		else (
-            // 			a++
-            // 			vardate++)
-            // 	similar for others
-            // place array results in table based on selectors
-            // place table in div
-            // }
-        })
-};
 
-var search = new KotakuSearch;
-$('.searchForm').on('submit', function(query) {
-    query.preventDefault();
-    var searchQuery = ($(".searchBox").val());
-    search.ShowResults(searchQuery);
+    KotakuSearch.prototype.ShowResults = function(query) {
+        var self = this;
+        $.when(
+            this.TagSearch(query)
+        ).then(
+            function(results) {
+                console.log(results);
+                var data = results.results.TagSearch;
+                $('.resultsAmount')[0].innerText = "There are " + data.length + " tag results for this query.";
+                // and " + searchData.length + " search results 
+                console.log(data); //showing all data returned
+                var ego = self;
 
-})
+                function arrayifyData(incomingData) {
+                    var dataResults = new Array(); // our concatenated Array of incomingData
+                    var dataResultsDates = new Array();
+                    var dataArraySpot = 0; // Spot we're saving to in articles
+                    var dateResultsArraySpot = 0 // Spot we're saving to in dates
+                    for (var i = incomingData.length - 1; i >= 0; i--) {
+                        // for (i = 0; i < incomingData.length; i--) {
+
+                        var fixedDate = ego.DateCheck(incomingData[i].Date.text); //result will be proper date
+                        if (i === (incomingData.length - 1)) {
+                            // if (i === 0) {
+
+                            dataResults[dataArraySpot] = {
+                                date: fixedDate,
+                                articles: 1
+                            };
+                            dataResultsDates[dateResultsArraySpot] = fixedDate;
+
+                        } else {
+                            if (dataResults[dataArraySpot].date === fixedDate) {
+                                dataResults[dataArraySpot].articles += 1;
+                            } else {
+                                dataArraySpot += 1;
+                                dataResults[dataArraySpot] = {
+                                    date: fixedDate,
+                                    articles: 1
+                                };
+                                dateResultsArraySpot += 1;
+                                dataResultsDates[dateResultsArraySpot] = fixedDate;
+                            };
+                        };
+                    };
+                    var combinedResults = [dataResults, dataResultsDates];
+                    return combinedResults;
+
+                };
+                var arrayifiedTagResults = arrayifyData(data);
+                // var arrayifiedSearchResults = arrayifyData(searchdata);
+                var taggedDataResults = arrayifiedTagResults[0];
+                // var searchDataResults = arrayifiedSearchResults[0];
+
+                var dateArray = new Array();
+
+                function addDates(array) {
+                    for (i = 0; i < array.length; i++) {
+                        dateArray.push(array[i]);
+                    };
+                    return;
+                };
+
+                addDates(arrayifiedTagResults[1]);
+                // addDates(arrayifiedSearchResults[1]);
+
+                //so all the dates are now in an array as strings
+
+                function arraySortByDate(a, b) {
+                    var dateA = new Date(a);
+                    var dateB = new Date(b);
+                    if (dateA > dateB) {
+                        return 1;
+                    } else if (dateA < dateB) {
+                        return -1;
+                    } else if (dateA === dateB) {
+                        dateArray.splice((dateArray.indexOf(dateA)), 1);
+                        return 0;
+                    };
+                };
+
+                dateArray = dateArray.sort(arraySortByDate);
+                dateArray = (function() {
+                    var oldDates = dateArray;
+                    dateArray = new Array();
+                    oldDates.forEach(function(date) {
+                        if (dateArray.indexOf(date) === -1) {
+                            dateArray.push(date);
+                        };
+                    });
+                    return dateArray;
+                }(dateArray)); // IIFE function that gets rid of dupes
+
+                //result should be an array with each day's result in its own spot.
+
+                $('.resultsDestination')[0].innerHTML = "<table class='resultsTable'><tr class='tableHeader'><td class='tableTitle'>Date</td><td class='tableTitle'>Number of Articles</td></tr>";
+                var tableHTML = '';
+                for (i = 0; i < dateArray.length; i++) { // for each new date
+                    var tableDate = dateArray[i]; // save the date to the variable
+                    var taggedNum = 0; // set up a variable for the taggedNum
+                    taggedDataResults.forEach(
+                        function(oneResult) {
+                            if (oneResult.date.indexOf(tableDate) !== -1) {
+                                var a = taggedDataResults.filter(function(object) {
+                                    return object.date === tableDate;
+                                });
+                                taggedNum = a[0].articles;
+                            }
+
+                        });
+
+                    // var articleNum = 0; // set up a variable for the taggedNum
+                    // searchDataResults.forEach(
+                    //     function(oneResult) {
+                    //         if (oneResult.date.indexOf(tableDate) !== -1) {
+                    //             var a = searchDataResults.filter(function(object) {
+                    //                 return object.date === tableDate;
+                    //             });
+                    //             articleNum = a[0].articles;
+                    //         }
+
+                    //     });
+
+                    tableHTML += "<tr><td class='date'>" + tableDate
+                    // + "</td><td class='articleNum'>" + articleNum 
+                    + "</td><td class='taggedNum'>" + taggedNum + "</td></tr>";
+                };
+                $('.resultsDestination').append(tableHTML);
+
+                // at this point, chart the data.
+
+            })
+    };
+
+    KotakuSearch.prototype.ShowPageviews = function() {
+        var choice = confirm('Depending on the number of results, this can take a few minutes.\n\n Are you sure you want to proceed?');
+        if (choice === true) {
+            // perform get on pageviews at one second intervals
+        }
+    };
+
+
+    var search = new KotakuSearch;
+    search.ShowResults('Destiny');
+    $('.pageviewButton').on('click', function() {
+        search.ShowPageviews();
+    });
+    // search.ShowResults(searchQuery);
+    // $('.searchForm').on('submit', function(query) {
+    //     query.preventDefault();
+    //     var searchQuery = ($(".searchBox").val());
+    //     search.ShowResults(searchQuery);
+
+    // })
 };
 
 window.onload = app;
+
+$.jqplot('chartdiv', [
+    [
+        [1, 21],
+        [3, 5.12],
+        [4, 13.1],
+        [7, 33.6],
+        [9, 85.9],
+        [10, 219.9]
+    ]
+]);
+
+// var line1 = [
+//     ['2008-09-30 4:00PM', 4],
+//     ['2008-10-30 4:00PM', 6.5],
+//     ['2008-11-30 4:00PM', 5.7],
+//     ['2008-12-30 4:00PM', 9],
+//     ['2009-01-30 4:00PM', 8.2]
+// ];
+// var plot1 = $.jqplot('.chartdiv', [line1], {
+//     title: 'Destiny Articles Over Time',
+//     axes: {
+//         xaxis: {
+//             renderer: $.jqplot.DateAxisRenderer
+//         }
+//     },
+//     series: [{
+//         lineWidth: 4,
+//         markerOptions: {
+//             style: 'square'
+//         }
+//     }]
+// })
